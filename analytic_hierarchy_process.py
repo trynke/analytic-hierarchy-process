6f6056b3f9fa89fe250bd3738dcfa5df47983425
Ekaterina <pirit544@gmail.com>
2020-11-13 22:02:42 +0500

    write basic logic of program

--- analytic_hierarchy_process.py
+++ analytic_hierarchy_process.py
@@ -1,2 +1,17 @@
 import numpy as np
 import pandas as pd
+
+def matrix(number):
+    pass
+
+def main():
+    number = str(input("How many criteria do you want to enter? Type an integer: "))
+    if number.isdigit():
+        matrix(number)
+    else:
+        print("Something went wrong. Let's try again")
+        main()
+
+
+if __name__ == "__main__":
+    main()
