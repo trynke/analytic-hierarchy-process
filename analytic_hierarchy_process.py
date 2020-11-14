3fcbb6cb6fc26f63be09d2cb67158e320e48c35a
Ekaterina <pirit544@gmail.com>
2020-11-14 20:59:27 +0500

    add function for pairwise matrix

--- analytic_hierarchy_process.py
+++ analytic_hierarchy_process.py
@@ -1,15 +1,26 @@
 import numpy as np
-import pandas as pd
 
 def matrix(number):
-    pass
+    """Function for creating a pairwise matrix"""
+    A = np.ones([number, number])
+    importance = [i for i in range(1, 10)]
+    for i in range(0, number):
+        for j in range(0, number):
+            if i < j:
+                a = str(input(f'How much more important is criterion {i} than criterion {j}? Type an integer from 1 to 9: '))
+                if a.isdigit() and float(a) in importance:
+                    A[i,j] = float(a)
+                    A[j, i] = 1 / float(a)
+                else:
+                    print("It is an incorrect value. Let's start again \n")
+                    main()
 
 def main():
     number = str(input("How many criteria do you want to enter? Type an integer: "))
     if number.isdigit():
-        matrix(number)
+        matrix(int(number))
     else:
-        print("Something went wrong. Let's try again")
+        print("Something went wrong. Let's try again \n")
         main()
 
 
